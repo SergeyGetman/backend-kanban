17d0f100027f09d4a872944cedc73dfe4005d896
Sergey Getman <sergeygetman2020@gmail.com>
2026-08-17 18:00:57 +0300

    added cors policy

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
-import express from "express"; 
+import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import route from "./routes/userRoute.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 7000;
 const MONGOURL = process.env.MONGO_URL;
