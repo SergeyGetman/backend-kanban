6c49a2394d6792acbc1dd27652b841611af16967
Sergey Getman <sergeygetman2020@gmail.com>
2026-08-17 18:02:35 +0300

    add axios, cors policy

--- client/src/getuser/User.tsx
+++ client/src/getuser/User.tsx
@@ -1,44 +1,72 @@
-import React from 'react'
-import "./user.css"
+import React, { useEffect, useState } from "react";
+import "./user.css";
+import axios from "axios";
+
+type IUsers = {
+  email: string;
+  name: string;
+  address: string;
+};
 
 const User = () => {
-  return (
-    <div className='userTable'>
-       <button type="button" className="btn btn-primary"> Add user
-        <i className="fa-solid fa-user-plus"></i>
-       </button>
-              <table className='table table-bordered'>
-                    <thead>
-                      <tr>
-                        <th scope='col'> SOL</th>
-                        <th scope='col'> NAME</th>
-                        <th scope='col'> Email</th>
-                        <th scope='col'> Adress</th>
-                        <th scope='col'> Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                          <td>1</td>
-                          <td>John</td>
-                          <td>john@gmail.com</td>
-                          <td>Canada</td>
-                          <td className='actionsButtons'>
-                            <button type="button" className="btn btn-info">
-                                     <i className="fa-solid fa-pen-to-square"></i>
-                            </button>
-                     
-                             <button type="button" className="btn btn-danger">   
-                                <i className="fa-solid fa-trash" ></i>
-                        
-                                </button>
-                        
-                             </td>
-                        </tr>
-                    </tbody>
-              </table>
-    </div>
-  )
-}
+  const [users, setUsers] = useState<IUsers[]>([]);
 
-export default User
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<IUsers[]>(
+          "http://localhost:8000/api/users",
+        );
+        setUsers(response.data);
+
+        console.log("trhis is responce", response);
+      } catch (error) {
+        console.error("ERROR fetching data");
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="userTable">
+      <button type="button" className="btn btn-primary">
+        Add user
+        <i className="fa-solid fa-user-plus"></i>
+      </button>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col"> SOL</th>
+            <th scope="col"> NAME</th>
+            <th scope="col"> Email</th>
+            <th scope="col"> Adress</th>
+            <th scope="col"> Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, idx) => {
+            return (
+              <tr>
+                <td>{idx + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.address}</td>
+                <td>{user.email}</td>
+                <td className="actionsButtons">
+                  <button type="button" className="btn btn-info">
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </button>
+
+                  <button type="button" className="btn btn-danger">
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default User;
